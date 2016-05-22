d38231a9ce36a9fa13d95d6929cf30067720f35d
ely <elycbiggs@yahoo.com>
2016-05-22 09:20:29 -0400

    profile updated

--- src/main/java/com/example/ely/drugfree/MainActivity.java
+++ src/main/java/com/example/ely/drugfree/MainActivity.java
@@ -23,10 +23,27 @@ public class MainActivity extends AppCompatActivity {
         //this is a test
 
         final Button bPanic = (Button) findViewById(R.id.bPanic);
-        final EditText etTestinfo = (EditText) findViewById(R.id.etTestinfo);
+        final EditText etPName = (EditText) findViewById(R.id.etPName);
+        final EditText etPEmergencyContact = (EditText) findViewById(R.id.etPEmergencyContact);
+        final EditText etPBio = (EditText) findViewById(R.id.etPBio);
+        final EditText etPJournal = (EditText) findViewById(R.id.etPJournal);
+
+
         final Button bResources = (Button) findViewById(R.id.bResources);
         final Button bChat = (Button) findViewById(R.id.bChat);
         final Button bFind = (Button) findViewById(R.id.bFind);
+        final Button bSubmit = (Button) findViewById(R.id.bSubmit);
+
+        SharedPreferences sp = getSharedPreferences("profileInfo", 0);
+
+        etPBio.getEditableText().clear();
+        etPBio.getEditableText().append(sp.getString("bio", ""));
+
+        etPName.getEditableText().append(sp.getString("username", ""));
+        etPEmergencyContact.getEditableText().append(sp.getString("emergencyContact", ""));
+
+
+
         //ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.SEND_SMS},1);
 
         bPanic.setOnClickListener(new View.OnClickListener()   {
